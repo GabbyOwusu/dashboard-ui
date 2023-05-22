d9b2e62ee883ab2cb1e0cbc59d3d2aca0b0b6c60
Gabriel Kofi Owusu <59026711+GabbyOwusu@users.noreply.github.com>
2023-05-22 17:51:22 +0000

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="flex">
+    <SideBar class="overflow-hidden" />
+    <MainBody />
+  </main>
+</template>
+
+<script>
+import MainBody from "~/components/MainBody.vue";
+import SideBar from "~/components/SideBar.vue";
+
+export default {
+  name: "IndexPage",
+  components: { SideBar, MainBody },
+};
+</script>
