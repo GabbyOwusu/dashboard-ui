de188b71cce6f4c2bd91d0d0308e36572b4aeb66
Gabriel Kofi Owusu <59026711+GabbyOwusu@users.noreply.github.com>
2023-05-22 17:51:13 +0000

    Create TextIconButton.vue

---
+++ components/TextIconButton.vue
@@ -0,0 +1,58 @@
+<template>
+  <button
+    :style="{
+      'background-color': `${fillColor}`,
+      'padding-left': `${fillColor ? '0.5rem' : ''}`,
+      'padding-right': `${fillColor ? '0.6rem' : ''}`,
+    }"
+    class="py-2 flex hover:bg-[#F0FBFB] rounded-[8px] w-full"
+  >
+    <div
+      :style="{
+        color: `${colored ? '#F0FBFB' : '#686868'}`,
+      }"
+      class="h-5 w-5 flex items-center justify-center rounded-md"
+    >
+      <img
+        v-if="leadingImage"
+        height="17"
+        width="17"
+        :src="require(`../assets/images/${leadingImage}.svg`)"
+        alt=""
+      />
+      <img
+        v-if="colored && !leadingImage"
+        height="12"
+        width="12"
+        src="~/assets/images/plus_icon.svg"
+        alt=""
+      />
+      <img
+        v-if="!colored && !leadingImage"
+        height="12"
+        width="12"
+        src="~/assets/images/plus_icon_grey.svg"
+        alt=""
+      />
+    </div>
+    <h1
+      :style="{
+        color: `${colored ? '#1DA19F' : '#989898'}`,
+      }"
+      class="ml-2 text-sm"
+    >
+      {{ label }}
+    </h1>
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    label: { type: String },
+    colored: { type: Boolean, default: false },
+    fillColor: { type: String },
+    leadingImage: { type: String },
+  },
+};
+</script>
