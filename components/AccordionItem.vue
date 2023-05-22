3a524b815a31ad84206f0a9028a32970d33c211f
Gabriel Kofi Owusu <59026711+GabbyOwusu@users.noreply.github.com>
2023-05-22 17:50:54 +0000

    Create AccordionItem.vue

---
+++ components/AccordionItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    class="flex w-full px-[0.125rem] py-1 items-center hover:text-gray-500"
+  >
+    <div class="mr-2" v-if="leading">
+      <h1>{{ leading }}</h1>
+    </div>
+    <div class="flex">
+      <h1 class="text-sm">
+        {{ label }}
+      </h1>
+    </div>
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    label: { type: String },
+    leading: { type: String, required: true },
+  },
+};
+</script>
