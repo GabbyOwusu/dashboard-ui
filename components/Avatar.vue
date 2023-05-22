1ecee3ee8dc0eb061f90666cd3304af3d9a226eb
Gabriel Kofi Owusu <59026711+GabbyOwusu@users.noreply.github.com>
2023-05-22 17:50:59 +0000

    Create Avatar.vue

---
+++ components/Avatar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="relative">
+    <div
+      :style="{
+        height: `${size}px`,
+        width: `${size}px`,
+        'border-color': `${borderColor}`,
+        'border-width': `${borderWidth}px`,
+      }"
+      class="rounded-[50%] bg-gray-500 border-solid"
+    >
+      <img
+        v-if="avatar"
+        class="rounded-[50%]"
+        height="50"
+        width="50"
+        :src="require(`../assets/images/${avatar}.png`)"
+      />
+    </div>
+    <div
+      v-if="showActive"
+      :style="{
+        'background-color': `${isActive ? '#15803D' : 'grey'}`,
+      }"
+      class="h-2 w-2 rounded-[50%] absolute bottom-2 right-0"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    size: { type: String, default: "28" },
+    avatar: { type: String },
+    borderColor: { type: String, default: "#9CA3AF" },
+    borderWidth: { type: String, default: "2.5" },
+    showActive: { type: Boolean, defatault: false },
+    isActive: { type: Boolean, defatault: false },
+  },
+};
+</script>
